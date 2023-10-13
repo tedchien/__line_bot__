82bece3f1b6fa83f0939abcedb845ccfeec224e2
tchien <34215494+tedchien@users.noreply.github.com>
2023-10-13 08:06:43 +0000

    modify

--- index.py
+++ index.py
@@ -23,6 +23,7 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    print(f"receive message:",event.message.text)
     message = TextSendMessage(text=event.message.text)
     line_bot_api.reply_message(event.reply_token, message)
 
